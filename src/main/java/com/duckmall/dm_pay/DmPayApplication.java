9d245799c6bcafd3a3d09a7c7b9e0facea8ee5c7
frank <1823427924@qq.com>
2022-08-19 18:01:30 +0800

    first commit

---
+++ src/main/java/com/duckmall/dm_pay/DmPayApplication.java
@@ -0,0 +1,13 @@
+package com.duckmall.dm_pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DmPayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/duckmall/dm_pay/DmPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duckmall.dm_pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
